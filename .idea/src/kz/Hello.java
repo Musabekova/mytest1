1c7072a201b56ff9470dcf2a5b150f767b6b2904
Bota Musabek <musabekova@mail.ru>
2017-10-03 03:25:07 +0600

    Ekinwi commit

--- .idea/src/kz/Hello.java
+++ .idea/src/kz/Hello.java
@@ -3,5 +3,7 @@ package kz;
 public class Hello {
     public static void main(String[] args){
         System.out.println("HELLO!");
+        System.out.println("Bul zerthanalyk jumystyn maksaty:");
+        System.out.println("Github-pen jumys jasap uyrenu.");
     }
 }
